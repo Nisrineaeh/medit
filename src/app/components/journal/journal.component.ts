82a82ddec0443596e6a5e527127d70157c1a414d
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-08-02 13:53:36 +0200

    creation component journal + html

--- src/app/components/journal/journal.component.ts
+++ src/app/components/journal/journal.component.ts
@@ -1,12 +1,30 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TechniqueService } from '../services/technique.service';
+import { Techniques } from 'src/app/models/techniques';
 
 @Component({
   selector: 'app-journal',
   templateUrl: './journal.component.html',
   styleUrls: ['./journal.component.css']
 })
-export class JournalComponent {
+export class JournalComponent implements OnInit{
+  techniques!: Techniques[];
+  message: string ="";
+  constructor(private techniquesService: TechniqueService) { }
+
+  ngOnInit() {
+    // Chargez les techniques de méditation lors de l'initialisation du composant
+    this.loadMeditationTechniques();
+  }
+
+  loadMeditationTechniques() {
+   
+      this.techniquesService.getTechnoiques().subscribe((technique) => {
+        this.techniques = technique;
+        console.log(this.techniques)
+      })
+    }
+  }
 
 
 
-} 
